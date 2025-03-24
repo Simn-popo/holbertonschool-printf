7958b1f3dc8cc57b629bfd7cc1acac3354820e60
Simn-popo <paulin.simon@live.fr>
2025-03-24 19:34:08 +0100

    main.h

--- main.h
+++ main.h
@@ -2,16 +2,9 @@
 #define main_h
 #include <stdarg.h>
 
-
-typedef struct main
-{
-	/* data */
-};
-
-
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(char c);
-int print_string(char *);
+int print_string(char *s);
 
 #endif
