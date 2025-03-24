694e7d914db671661d9b139dd55682a3dace2ece
Simn-popo <paulin.simon@live.fr>
2025-03-24 18:23:50 +0100

    main.h

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+
+
+typedef struct main
+{
+	/* data */
+};
+
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int printpercent(va_list args);
+
+#endif
