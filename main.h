08876c516dec75c88de5d60696a8df398a0e757d
Simn-popo <paulin.simon@live.fr>
2025-03-25 12:11:35 +0100

    main.h

--- main.h
+++ main.h
@@ -2,9 +2,11 @@
 #define main_h
 #include <stdarg.h>
 
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(char c);
 int print_string(char *s);
+int print_percent(char c);
 
 #endif
