7ed2188f2c64bb597eff20cfb2d9db6e68bc0a6d
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-26 20:48:48 +0100

    main.h

--- main.h
+++ main.h
@@ -1,14 +1,29 @@
-main.h 
+#ifndef MAIN_H
+#define MAIN_H
 
-#ifndef main_h
-#define main_h
 #include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
 
-
+char _putchar(char character);
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_char(char c);
-int print_string(char *s);
-int print_percent(char c);
+int print_char(va_list args);
+int print_str(va_list args);
+int print_modulo(va_list args);
+int print_integer(va_list args);
+int get_specifier(const char format, va_list args);
+
+/**
+ * struct spe - a structure that check specifier
+ * @specifier: the character of the specifier
+ * @f: a pointer function pointing to _printf
+ */
+
+typedef struct spe
+{
+	char specifier;
+	int (*f)(va_list);
+
+} specifier_t;
 
 #endif
