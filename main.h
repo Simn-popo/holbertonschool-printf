2dbbf8e1f977d0fd5d338ee0b658560a26e758ba
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-26 20:38:40 +0100

    changed back main.h

--- main.h
+++ main.h
@@ -1,19 +1,14 @@
+main.h 
+
 #ifndef main_h
 #define main_h
 #include <stdarg.h>
 
-typedef struct op
-{
-	char *specifier;
-	int (*func)(va_list);
-}op_t;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_integer(va_list args);
-int get_specifier(char specifier, va_list args);
+int print_char(char c);
+int print_string(char *s);
+int print_percent(char c);
 
 #endif
