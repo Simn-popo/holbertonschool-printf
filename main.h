575bbd31586120704f7ed166d81a5a98d5d191f2
Simn-popo <paulin.simon@live.fr>
2025-03-26 15:02:57 +0100

    main.h

--- main.h
+++ main.h
@@ -2,11 +2,17 @@
 #define main_h
 #include <stdarg.h>
 
+typedef struct op
+{
+	char *specifier;
+	int (*func)(va_list);
+}op_t;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(char c);
 int print_string(char *s);
 int print_percent(char c);
+int print_integer(int n);
 
 #endif
