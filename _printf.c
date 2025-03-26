953f3ef718e500c8495796114245a776282714f7
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-26 20:14:30 +0100

    change a few errors in code

--- _printf.c
+++ _printf.c
@@ -1,58 +1,64 @@
 #include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
 
 /**
- * print_specifier - Handles the correct format specifier.
- * @specifier: The format specifier character.
- * @args: The list of arguments to process.
+ * _printf - Function that produces output according to a format
+ * @format: The format string
+ * @...: The variable arguments
  *
- * Return: The number of characters printed, or -1 on failure.
- */
-int print_specifier(char specifier, va_list args)
-{
-	if (specifier == 'c')
-		return (_putchar(va_arg(args, int)));
-	if (specifier == 's')
-		return (print_string(va_arg(args, char *)));
-	if (specifier == '%')
-		return (_putchar('%'));
-	return (_putchar('%') + _putchar(specifier)); 
-}
-
-/**
- * _printf - Custom implementation of printf
- * @format: The format string containing conversion specifiers.
- *
- * Return: Number of characters printed, or -1 on error.
+ * Return: The number of characters printed
  */
 int _printf(const char *format, ...)
 {
-	int x, len = 0;
-	va_list args;
+    int len = 0;
+    int x;
+    va_list args;
 
-	if (!format)
-		return (-1);
-	if (format[0] == '%' && format[1] == '\0') 
-		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && format[2] == '\0') 
+    /* Array of supported specifiers and their corresponding functions */
+    specifier_t specifiers[] = {
+        {'c', print_char},
+        {'s', print_string},
+        {'%', print_percent},
+        {'d', print_integer},
+        {'i', print_integer},
+        {'p', print_pointer},
+        {'\0', NULL} /* End of list */
+    };
 
-	va_start(args, format);
-	for (x = 0; format[x] != '\0'; x++)
-	{
-		if (format[x] == '%')
-		{
-			x++; 
-			if (format[x] == '\0') 
-				return (-1);
-			len += print_specifier(format[x], args);
-		}
-		else
-		{
-			_putchar(format[x]);
-			len++;
-		}
-	}
-	va_end(args);
-	return (len);
+    va_start(args, format);
+
+    if (format == NULL)
+        return (-1);
+
+    /* Loop through the format string */
+    for (x = 0; format[x] != '\0'; x++)
+    {
+        if (format[x] == '%')
+        {
+            int specifier_found = 0;
+            /* Check for valid specifier */
+            for (int i = 0; specifiers[i].spec != '\0'; i++)
+            {
+                if (format[x + 1] == specifiers[i].spec)
+                {
+                    len += specifiers[i].f(args);
+                    specifier_found = 1;
+                    break;
+                }
+            }
+            /* If no specifier found, print the '%' character */
+            if (!specifier_found)
+            {
+                len += _putchar(format[x]);
+            }
+            x++;  /* Skip the specifier */
+        }
+        else
+        {
+            len += _putchar(format[x]);
+        }
+    }
+
+    va_end(args);
+    return (len);
 }
