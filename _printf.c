c4e560a7e316bed2c0e73a0683e02e771230f4b8
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-26 20:10:03 +0100

    _printf.c

--- _printf.c
+++ _printf.c
@@ -1,40 +1,52 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Fonction that prints simple string or follow conversion specifiers
- *	%c : specifier that prints a single character
- *	%s : specifier that prints a string of characters
- *	%% : specifier that prints a modulo
- *	%d : specifier that prints a decimal base 10 number
- *	%i : specifier that prints an integer base 10
- * @format: number of arguments in the array
- * Return: the number of character printed if success, or -1 if failed
+ * print_specifier - Handles the correct format specifier.
+ * @specifier: The format specifier character.
+ * @args: The list of arguments to process.
+ *
+ * Return: The number of characters printed, or -1 on failure.
  */
+int print_specifier(char specifier, va_list args)
+{
+	if (specifier == 'c')
+		return (_putchar(va_arg(args, int)));
+	if (specifier == 's')
+		return (print_string(va_arg(args, char *)));
+	if (specifier == '%')
+		return (_putchar('%'));
+	return (_putchar('%') + _putchar(specifier)); 
+}
 
+/**
+ * _printf - Custom implementation of printf
+ * @format: The format string containing conversion specifiers.
+ *
+ * Return: Number of characters printed, or -1 on error.
+ */
 int _printf(const char *format, ...)
 {
 	int x, len = 0;
 	va_list args;
 
-	va_start(args, format);
+	if (!format)
+		return (-1);
+	if (format[0] == '%' && format[1] == '\0') 
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && format[2] == '\0') 
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-	{
-		return (-1);
-	}
-	if (format[0] == '%' && format[1] == ' ' && format[2] == '\0')
-	{
-		return (-1);
-	}
-	for (x = 0; format[x] != '\0' ; x++)
+	va_start(args, format);
+	for (x = 0; format[x] != '\0'; x++)
 	{
 		if (format[x] == '%')
 		{
-			len += get_specifier(format[x + 1], args);
-			x++;
+			x++; 
+			if (format[x] == '\0') 
+				return (-1);
+			len += print_specifier(format[x], args);
 		}
-		
 		else
 		{
 			_putchar(format[x]);
