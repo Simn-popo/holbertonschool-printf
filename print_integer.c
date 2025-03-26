d371ce32fc0c8f73a98530fbc3d93c6e150dea4f
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-26 15:58:04 +0100

    changed code after errors in print_integer

--- print_integer.c
+++ print_integer.c
@@ -1,37 +1,33 @@
 #include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
 
 /**
- * get_specifier - Function that checks for the right specifier tu use
- * %c, %s, %%, %d, %i
- * @format: the string set in _printf function
- * @args: a the list set in _printf function
- * Return: the length of the called pointer function pointed by f
+ * print_integer - function that prints negative and positive integers
+ * @n : The integer to print
+ * Return: Number of digits printed
  */
 
-int get_specifier(const char format, va_list args)
+int print_integer(va_list args)
 {
-	int x;
-	specifier_t spe[] = {
-		{'c', print_char},
-		{'s', print_str},
-		{'%', print_modulo},
-		{'d', print_integer},
-		{'i', print_integer},
-		{'\0', NULL},
-	};
-	/** initialise every argument of the the array spe */
-	x = 0;
-	while (spe[x].specifier)
+	int count = 0;
+	unsigned int num;
+
+	if (n < 0)
 	{
-		if (format == spe[x].specifier)
-		{
-			return (spe[x].f(args));
-		}
-		x++;
+		_putchar ('-');
+		count++;
+		num = -n;
 	}
-	/** incrementation of the array until it is the null character */
-	/** then checked if one of the argument of format is the same one of spe **/
-	_putchar('%');
-	_putchar(format);
-	return (2);
+	else
+		num = n;
+
+	if (num / 10)
+		count += print_integer(num / 10);
+
+	_putchar((num % 10) + '0');
+	count++;
+
+Return: (count);
+
 }
