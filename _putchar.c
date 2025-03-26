df3e246707c9e9b97c7d7d48c4cc09aadc5d4471
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-26 20:49:56 +0100

    changed name for putchar

---
+++ _putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the charactere c to stdout
+ * @c: charatere to print
+ * Return: 1 on success
+ * on error, -1 is returned
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
