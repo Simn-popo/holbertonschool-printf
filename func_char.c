3833982bdf7786caa7f69261ad5c75f7f93a9883
azbin-spec <dydouhabdellah82@gmail.com>
2025-03-26 20:43:10 +0100

    changed func_char.c

--- func_char.c
+++ func_char.c
@@ -1,22 +1,14 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * print_char - Prints a character
- * @args: The argument list containing the character
- *
- * Return: The number of characters printed
+ * print_char - print a charactere to stdout
+ * @c: charactere to print
+ * Return: charatere(s) whitout the null byte
  */
-<<<<<<< HEAD
 
 int print_char(va_list args)
 {
-	char c = va_arg(args, int);
 	_putchar(c);
 	return (1);
-=======
-int print_char(va_list args)
-{
-    _putchar(va_arg(args, int));
-    return (1);
->>>>>>> f4f01afc6e36518fe5a2981e5ad2100e4e372f0c
 }
