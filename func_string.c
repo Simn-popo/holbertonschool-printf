6150626ed9ee58c766bced500ca7d585837dfa51
Simn-popo <paulin.simon@live.fr>
2025-03-28 15:41:43 +0100

    add: description

--- func_string.c
+++ func_string.c
@@ -12,13 +12,13 @@ int _string(va_list args)
 	char *s = va_arg(args, char *);
 	int sum = 0, i = 0;
 
-	if (s == NULL)
-		s = "(null)";
+	if (s == NULL)/*check if its NULL*/
+		s = "(null)";/*return the string exercice*/
 	if (s != NULL)
 	{
 		while (s[i])
 		{
-			_putchar(s[i]);
+			_putchar(s[i]);/*print the string*/
 			sum++;
 			i++;
 		}
